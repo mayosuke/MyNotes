c8e8f6997fed74c1d3837ac30397db6ffd72f4ed
mayosuke <mayo.suke@gmail.com>
2012-07-16 16:46:40 +0900

    updating. #1

--- src/jp/mayosuke/android/mynotes/NoteDetailActivity.java
+++ src/jp/mayosuke/android/mynotes/NoteDetailActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class NoteDetailActivity extends Activity {
     private static final String TAG = NoteDetailActivity.class.getSimpleName();
+
+    private static final String INSTANCE_STATE_CONTENT = "jp.mayosuke.android.mynotes.NoteDetailActivity.mContent";
+
+    private EditText mContent;
 
     @TargetApi(11)
     @Override
@@ -17,6 +22,10 @@ public class NoteDetailActivity extends Activity {
         Log.v(TAG, "onCreate():savedInstanceState=" + savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+        mContent = (EditText)findViewById(R.id.note_detail_content);
+        if (mContent == null) {
+            throw new NullPointerException("You should provide EditText for this activity.");
+        }
     }
 
     @Override
@@ -70,12 +79,15 @@ public class NoteDetailActivity extends Activity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         Log.v(TAG, "onSaveInstanceState():outState=" + outState);
+        outState.putCharSequence(INSTANCE_STATE_CONTENT, mContent.getText());
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         Log.v(TAG, "onRestoreInstanceState():savedInstanceState=" + savedInstanceState);
+        final CharSequence content = savedInstanceState.getCharSequence(INSTANCE_STATE_CONTENT);
+        mContent.setText(content);
         super.onRestoreInstanceState(savedInstanceState);
     }
 
