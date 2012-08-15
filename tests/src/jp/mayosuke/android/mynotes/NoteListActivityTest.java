4bef4a1f93ba615c11ae15a51288080f59c3d3f6
mayosuke <mayo.suke@gmail.com>
2012-08-15 09:53:58 +0900

    Adding test codes.

---
+++ tests/src/jp/mayosuke/android/mynotes/NoteListActivityTest.java
@@ -0,0 +1,15 @@
+package jp.mayosuke.android.mynotes;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class NoteListActivityTest extends ActivityInstrumentationTestCase2<NoteListActivity> {
+
+    public NoteListActivityTest(Class<NoteListActivity> activityClass) {
+        super(activityClass);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void testDummy() {
+        assertNotNull(null);
+    }
+}
